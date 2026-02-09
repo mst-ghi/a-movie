98f04f82057376154490af9ee65bff9957500a8b
Mostafa Gholami <mostafagholamidev@gmail.com>
2026-02-09 22:18:09 +0330

    source updated
    Signed-off-by: Mostafa Gholami <mostafagholamidev@gmail.com>

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 # A-Movie
 
-A-Movie project
+Welcome to the A-Movie API! This service provides a comprehensive interface for accessing a vast database of movies. You can search for specific titles, browse lists with various filters, and retrieve detailed information, including summaries, ratings, and poster images. This documentation will guide you through all the available endpoints and how to use them effectively.
 
 
 ## Author
--- core/swagger/swagger.go
+++ core/swagger/swagger.go
@@ -9,8 +9,8 @@ import (
 )
 
 func RegisterSwagger(router *gin.RouterGroup) {
-	docs.SwaggerInfo.Title = "Your app name"
-	docs.SwaggerInfo.Description = "Your swagger description"
+	docs.SwaggerInfo.Title = "A-Movie"
+	docs.SwaggerInfo.Description = "Welcome to the A-Movie API! This service provides a comprehensive interface for accessing a vast database of movies. You can search for specific titles, browse lists with various filters, and retrieve detailed information, including summaries, ratings, and poster images. This documentation will guide you through all the available endpoints and how to use them effectively."
 	docs.SwaggerInfo.Version = "0.1.0"
 
 	router.GET(
